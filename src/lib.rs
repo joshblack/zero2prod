128677a93cd06c5e613908085409b6f9fc4fa329
Josh Black <josh@josh.black>
2021-08-02 20:45:07 -0500

    chore: remove commented code

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,3 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
 
     Ok(server)
 }
-
-// pub async fn run() -> std::io::Result<()> {
-// HttpServer::new(|| {
-// App::new()
-// .route("/health_check", web::get().to(health_check))
-// .route("/", web::get().to(greet))
-// .route("/{name}", web::get().to(greet))
-// })
-// .bind("127.0.0.1:8000")?
-// .run()
-// .await
-// }
